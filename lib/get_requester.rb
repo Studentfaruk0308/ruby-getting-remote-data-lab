374acbf9657a88f82e76ca09e017eebaa5ac812b
faruk <studentfaruk0308@gmail.com>
2022-08-04 14:19:22 +1000

    After Completion

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,41 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize(url)
+        @url=url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+    def parse_json
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        JSON.parse(response.body) 
+    end
+
+end
+
+# class GetRequester
+
+#     def initialize(url)
+#         @url=url
+#     end
+
+#     def parse_json
+#         uri = URI.parse(@url)
+#         response = Net::HTTP.get_response(uri)
+#         JSON.parse(response.body) 
+#     end
+# end
